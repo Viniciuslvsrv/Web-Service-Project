e0c95089fa78981ca58763a202a2d6044214a2c9
Vinicius Alves Saraiva <vinicius.lvsrv@gmail.com>
2022-03-22 09:26:03 -0300

    Projeto criado

---
+++ ProjetoWebService/src/test/java/com/vinicius/ProjetoWebService/ProjetoWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.ProjetoWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
